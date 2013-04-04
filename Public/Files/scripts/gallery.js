e82256e7102a65a8ff2030e44291baaef76f3613
Dmitry Khomutov <poisoncorpsee@gmail.com>
2013-04-04 23:59:29 +0700

    bugfix

--- Public/Files/scripts/gallery.js
+++ Public/Files/scripts/gallery.js
@@ -15,11 +15,15 @@ $(document).ready (function(){
     {
         var ss = file_path + 'pictures/xmin/' + $(this).attr('id') + '-min.jpg';
         $(this).attr('src', ss);
+
+	    $(this).parents('img-box').css('background-color', 'black');
     });
 
     $('.gr-col').mouseout (function()
     {
         var ss = file_path + 'pictures/xgray/' + $(this).attr('id') + '-gray.jpg';
         $(this).attr('src', ss);
+
+	    $(this).parents('img-box').css('background-color', 'transporent');
     });
 });
