1d55542a3f1fbf208aaa425cc2cc9d932031a3fd
Dmitry Khomutov <poisoncorpsee@gmail.com>
2014-12-07 02:58:13 +0600

    Revert versions

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,8 @@ CYAN='\033[0;36m' #  ${CYAN}     # blue color
 
 PROJECT='corpsee.com'
 BASE_DIR='/var/www'
-VERSION='v18'
-LAST_VERSION='v17'
+VERSION='v17'
+LAST_VERSION='v16'
 MODE='production' # production|debug
 
 PROJECT_DIR="$BASE_DIR/$PROJECT"
