56b9b59911887ba42889caf5a2a8cbaddae39b6f
Dmitry Khomutov <poisoncorpsee@gmail.com>
2015-03-27 21:52:41 +0600

    Added migration to create "projects" table

--- Application/Migrations/20150322163941_init_migration.php
+++ Application/Migrations/20150322163941_init_migration.php
@@ -2,70 +2,87 @@
 
 use Phinx\Migration\AbstractMigration;
 
+/**
+ * Class InitMigration
+ */
 class InitMigration extends AbstractMigration
 {
     public function change()
     {
-        $table = $this->table('pages');
-        $table
-            ->addColumn('alias', 'text')
-            ->addIndex('alias', ['unique' => true])
-            ->create();
+        if (!$this->hasTable('pages')) {
+            $table = $this->table('pages');
+            $table
+                ->addColumn('alias', 'text')
+                ->addIndex('alias', ['unique' => true])
+                ->create();
+        }
 
-        $table = $this->table('pages_content');
-        $table
-            ->addColumn('language', 'text', ['default' => 'ru'])
-            ->addColumn('page_id', 'integer')
-            ->addColumn('title', 'text', ['default' => ''])
-            ->addColumn('description', 'text', ['default' => ''])
-            ->addColumn('keywords', 'text', ['default' => ''])
-            ->create();
+        if (!$this->hasTable('pages_content')) {
+            $table = $this->table('pages_content');
+            $table
+                ->addColumn('language', 'text', ['default' => 'ru'])
+                ->addColumn('page_id', 'integer')
+                ->addColumn('title', 'text', ['default' => ''])
+                ->addColumn('description', 'text', ['default' => ''])
+                ->addColumn('keywords', 'text', ['default' => ''])
+                ->create();
+        }
 
-        $table = $this->table('pictures');
-        $table
-            ->addColumn('title', 'text')
-            ->addColumn('image', 'text')
-            ->addColumn('description', 'text', ['default' => ''])
-            ->addColumn('create_date', 'timestamp', ['timezone' => true, 'null' => true])
-            ->addColumn('post_date', 'timestamp', ['timezone' => true])
-            ->addColumn('modify_date', 'timestamp', ['timezone' => true])
-            ->addIndex('title', ['unique' => true])
-            ->addIndex('image', ['unique' => true])
-            ->create();
+        if (!$this->hasTable('pictures')) {
+            $table = $this->table('pictures');
+            $table
+                ->addColumn('title', 'text')
+                ->addColumn('image', 'text')
+                ->addColumn('description', 'text', ['default' => ''])
+                ->addColumn('create_date', 'timestamp', ['timezone' => true, 'null' => true])
+                ->addColumn('post_date', 'timestamp', ['timezone' => true])
+                ->addColumn('modify_date', 'timestamp', ['timezone' => true])
+                ->addIndex('title', ['unique' => true])
+                ->addIndex('image', ['unique' => true])
+                ->create();
+        }
 
-        $table = $this->table('tags');
-        $table
-            ->addColumn('tag', 'text')
-            ->addColumn('post_date', 'timestamp', ['timezone' => true])
-            ->addColumn('modify_date', 'timestamp', ['timezone' => true])
-            ->addIndex('tag', ['unique' => true])
-            ->create();
+        if (!$this->hasTable('tags')) {
+            $table = $this->table('tags');
+            $table
+                ->addColumn('tag', 'text')
+                ->addColumn('post_date', 'timestamp', ['timezone' => true])
+                ->addColumn('modify_date', 'timestamp', ['timezone' => true])
+                ->addIndex('tag', ['unique' => true])
+                ->create();
+        }
 
-        $table = $this->table('pictures_tags');
-        $table
-            ->addColumn('picture_id', 'integer')
-            ->addColumn('tag_id', 'integer')
-            ->create();
+        if (!$this->hasTable('pictures_tags')) {
+            $table = $this->table('pictures_tags');
+            $table
+                ->addColumn('picture_id', 'integer')
+                ->addColumn('tag_id', 'integer')
+                ->create();
+        }
 
-        $table = $this->table('last_modify');
-        $table
-            ->addColumn('table', 'text')
-            ->addColumn('modify_date', 'timestamp', ['timezone' => true])
-            //->addIndex('table', ['unique' => true])
-            ->create();
+        if (!$this->hasTable('last_modify')) {
+            $table = $this->table('last_modify');
+            $table
+                ->addColumn('table', 'text')
+                ->addColumn('modify_date', 'timestamp', ['timezone' => true])
+                //->addIndex('table', ['unique' => true])
+                ->create();
+        }
 
-        $table = $this->table('pull_requests');
-        $table
-            ->addColumn('repository', 'text')
-            ->addColumn('number', 'integer')
-            ->addColumn('title', 'text', ['default' => ''])
-            ->addColumn('body', 'text', ['default' => ''])
-            ->addColumn('status', 'text')
-            ->addColumn('commits', 'integer')
-            ->addColumn('additions', 'integer')
-            ->addColumn('deletions', 'integer')
-            ->addColumn('files', 'integer')
-            ->addColumn('create_date', 'timestamp', ['timezone' => true])
-            ->create();
+        if (!$this->hasTable('pull_requests')) {
+            $table = $this->table('pull_requests');
+            $table
+                ->addColumn('repository', 'text')
+                ->addColumn('number', 'integer')
+                ->addColumn('title', 'text', ['default' => ''])
+                ->addColumn('body', 'text', ['default' => ''])
+                ->addColumn('status', 'text')
+                ->addColumn('commits', 'integer')
+                ->addColumn('additions', 'integer')
+                ->addColumn('deletions', 'integer')
+                ->addColumn('files', 'integer')
+                ->addColumn('create_date', 'timestamp', ['timezone' => true])
+                ->create();
+        }
     }
 }
---
+++ Application/Migrations/20150327153818_add_projects_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+/**
+ * Class AddProjectsTable
+ */
+class AddProjectsTable extends AbstractMigration
+{
+    public function change()
+    {
+        if (!$this->hasTable('projects')) {
+            $table = $this->table('projects');
+            $table
+                ->addColumn('title', 'text')
+                ->addColumn('description', 'text', ['default' => ''])
+                ->addColumn('link', 'text', ['default' => ''])
+                ->addColumn('role', 'text', ['default' => ''])
+                ->addColumn('image', 'text', ['default' => ''])
+                ->addColumn('post_date', 'timestamp', ['timezone' => true])
+                ->addColumn('modify_date', 'timestamp', ['timezone' => true])
+                ->addIndex('title', ['unique' => true])
+                ->create();
+        }
+    }
+}
