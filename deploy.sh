eb77b44f862428490b5ac8897b157ba3c96f9ccd
Dmitry Khomutov <poisoncorpsee@gmail.com>
2015-04-23 18:11:01 +0600

    Fixed deployment script

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ MODE='production' # production|debug
 PROJECT_DIR="$BASE_DIR/$PROJECT"
 BACKUP_DIR="/var/backups/$PROJECT"
 
-POSTGRESQL_USER="corpsee_com"
+POSTGRESQL_USER="corpsee.com"
 POSTGRESQL_PASSWORD="password"
 POSTGRESQL_DBNAME="corpsee_com_db"
 
@@ -36,16 +36,15 @@ release ()
     [ -d "$PROJECT"-"$VERSION" ] && rm -rf "$PROJECT"-"$VERSION"
     git clone git@github.com:corpsee/corpsee-com.git "$PROJECT"-"$VERSION"
 
-    cd "$BASE_DIR"/"$PROJECT"-"$VERSION"
+    cd "$PROJECT_DIR"-"$VERSION"
     git checkout -f "$VERSION"
 
-    sudo composer selfupdate
     composer install
 
     rm -rf ./.git
     rm -rf ./.gitignore
 
-    mkdir -p ./session
+    mkdir -p ./sessions
     mkdir -p ./temp
 
     sed -e "s:${POSTGRESQL_USER}:${POSTGRESQL_USER}:g;s:${POSTGRESQL_PASSWORD}:${POSTGRESQL_PASSWORD}:g;s:${POSTGRESQL_DBNAME}:${POSTGRESQL_DBNAME}:g" ./Application/configs/config."$MODE".php > ./Application/configs/config.php
@@ -65,27 +64,25 @@ release ()
     [ ! -d ./www/slides ]               && mkdir -p ./www/slides
     [ ! -d ./www/yanka ]                && mkdir -p ./www/yanka
 
-    cp -fv "$PROJECT_DIR"/www/files/posts/*          ./www/files/posts/
-    cp -fv "$PROJECT_DIR"/www/files/pictures/x/*     ./www/files/pictures/x/
-    cp -fv "$PROJECT_DIR"/www/files/pictures/xgray/* ./www/files/pictures/xgray/
-    cp -fv "$PROJECT_DIR"/www/files/pictures/xmin/*  ./www/files/pictures/xmin/
-    cp -fv "$PROJECT_DIR"/www/slides/*               ./www/slides/
-    cp -fv "$PROJECT_DIR"/www/yanka/*                ./www/yanka/
+    [ ! -d "$PROJECT_DIR"/www/files/posts ]          && cp -fv "$PROJECT_DIR"/www/files/posts/*          ./www/files/posts/
+    [ ! -d "$PROJECT_DIR"/www/files/pictures/x ]     && cp -fv "$PROJECT_DIR"/www/files/pictures/x/*     ./www/files/pictures/x/
+    [ ! -d "$PROJECT_DIR"/www/files/pictures/xgray ] && cp -fv "$PROJECT_DIR"/www/files/pictures/xgray/* ./www/files/pictures/xgray/
+    [ ! -d "$PROJECT_DIR"/www/files/pictures/xmin ]  && cp -fv "$PROJECT_DIR"/www/files/pictures/xmin/*  ./www/files/pictures/xmin/
+    [ ! -d "$PROJECT_DIR"/www/slides ]               && cp -fv "$PROJECT_DIR"/www/slides/*               ./www/slides/
+    [ ! -d "$PROJECT_DIR"/www/yanka ]                && cp -fv "$PROJECT_DIR"/www/yanka/*                ./www/yanka/
 
     chmod 774 ./console
 
     sudo disable-host "$PROJECT"
 
-    [ ! -d "$BACKUP_DIR" ] && sudo mkdir -p "$BACKUP_DIR"
+    [ -d "$PROJECT_DIR" ] && tar czf "$PROJECT_DIR"."$LAST_VERSION".tar.gz "$PROJECT_DIR"
+    [ -d "$PROJECT_DIR" ] && rm -rf  "$PROJECT_DIR"
 
-    [ -d "$BASE_DIR"/"$PROJECT" ] && tar czf "$BACKUP_DIR"/"$PROJECT"."$LAST_VERSION".tar.gz "$BASE_DIR"/"$PROJECT"
-    [ -d "$BASE_DIR"/"$PROJECT" ] && rm -rf  "$BASE_DIR"/"$PROJECT"
+    mkdir -p "$PROJECT_DIR"
+    mv -f    "$PROJECT_DIR"-"$VERSION"/* "$PROJECT_DIR"
+    rm -rf   "$PROJECT_DIR"-"$VERSION"
 
-    mkdir -p "$BASE_DIR"/"$PROJECT"
-    mv -f    "$BASE_DIR"/"$PROJECT"-"$VERSION"/* "$BASE_DIR"/"$PROJECT"
-    rm -rf   "$BASE_DIR"/"$PROJECT"-"$VERSION"
-
-    cd "$BASE_DIR"/"$PROJECT"
+    cd "$PROJECT_DIR"
 
     ./console assets:compile
     ./console migrations:migrate
