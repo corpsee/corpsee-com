f560796e772be05e1051ade948e1d4c58269c950
Dmitry Khomutov <poisoncorpsee@gmail.com>
2015-05-01 23:27:56 +0600

    Fixed PHPUnit and updated dependencies

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+define('START_TIME', microtime(true));
+define('START_MEMORY', memory_get_usage());
+
+define('ROOT_PATH', dirname(__DIR__) . '/');
+define('APPLICATION_PATH', ROOT_PATH . 'src/');
+define('CONFIG_PATH', APPLICATION_PATH . 'configs/');
+define('PUBLIC_PATH', ROOT_PATH . 'www/');
+define('FILE_PATH', PUBLIC_PATH . 'files/');
+define('FILE_PATH_URL', '/files/');
+
+require_once ROOT_PATH . 'vendor/autoload.php';
