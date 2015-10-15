d4ffe0d3749013b702f8a544ff688a380978adc3
Dmitry Khomutov <poisoncorpsee@gmail.com>
2015-10-15 20:22:30 +0600

    Fixed deploy script (session dir)

--- deploy/release.sh
+++ deploy/release.sh
@@ -77,8 +77,8 @@ chmod 774 ./console
 sudo disable-host -h "${PROJECT}" -y
 
     if [ -d "${PROJECT_DIR}" ]; then
-        tar czf "${BACKUP_DIR}/${PROJECT}"."${CURRENT_DATE}"."${CURRENT_TIMESTAMP}".tar.gz "${PROJECT_DIR}"
-        rm -rf  "${PROJECT_DIR}"/*
+        sudo tar czf "${BACKUP_DIR}/${PROJECT}"."${CURRENT_DATE}"."${CURRENT_TIMESTAMP}".tar.gz "${PROJECT_DIR}"
+        sudo rm -rf  "${PROJECT_DIR}"/*
     else
         mkdir -p "${PROJECT_DIR}"
     fi
